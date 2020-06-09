0023bc050044b9679f0e2da07561d6e91045c5ed
ian-SmartAg <isu@smart-ag.com>
2020-06-09 17:38:51 -0500

    Debugging for 206.

---
+++ Leetcode/206_reverse_linked_list.cpp
@@ -0,0 +1,77 @@
+#include <iostream>
+using namespace std;
+//   Definition for singly-linked list.
+struct ListNode {
+    int val;
+    ListNode *next;
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
+
+class Solution
+{
+public:
+    ListNode *reverseList(ListNode *head)
+    {
+        ListNode *temp = head;
+        ListNode *nextNode = nullptr;
+        ListNode *prev = temp->next;
+        while( temp != nullptr)
+        {
+            if(temp != nullptr){
+                cout << "In temp: " << temp->val << endl;
+            }
+            // if( prev != nullptr)
+            // {
+            //     cout << "In temp: " << temp->val << endl;
+            // }
+            // if (next != nullptr)
+            // {
+            //     cout << "In temp: " << temp->val << endl;
+            // }
+
+            temp->next = nextNode;
+            cout << "111 " << endl;
+            nextNode = temp;
+            cout << "222 " << endl;
+            temp = prev;
+            cout << "333 " << endl;
+            if(temp->next == nullptr){
+                cout << "TEMP next is null" << endl;
+            }
+            if(prev != nullptr)
+            {
+                cout << "555" << endl;
+                prev = prev->next;
+                cout << "666" << endl;
+            }
+            // prev = prev->next;
+            
+            cout << "444 " << endl;
+        }
+        cout << "888"<< endl;
+        return nextNode;
+    }
+};
+
+int main() 
+{
+    ListNode test1(1);
+    ListNode test2(2, &test1);
+    ListNode test3(3, &test2);
+    ListNode test4(4, &test3);
+    ListNode test5(5, &test4);
+    ListNode *view = &test5;
+
+    Solution sol;
+    view = sol.reverseList(view);
+    // while( view != nullptr )
+    // {
+    //     cout << view->val << endl;
+    //     view = view->next;
+    //     cout << "Dammm" << endl;
+    // }
+
+
+}
