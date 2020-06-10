0e8124a8ea037c6b8914f366d62f23274a6705be
Ian Su <iansu81@gmail.com>
2020-06-09 21:57:19 -0500

    Working!

--- Leetcode/206_reverse_linked_list.cpp
+++ Leetcode/206_reverse_linked_list.cpp
@@ -32,46 +32,47 @@ public:
             // }
 
             temp->next = nextNode;
-            cout << "111 " << endl;
+            // cout << "111 " << endl;
             nextNode = temp;
-            cout << "222 " << endl;
+            // cout << "222 " << endl;
             temp = prev;
-            cout << "333 " << endl;
-            if(temp->next == nullptr){
-                cout << "TEMP next is null" << endl;
-            }
+            // cout << "333 " << endl;
+            // if(temp == nullptr){
+            //     cout << "TEMP next is null" << endl;
+            // }
+            // cout << "999" << endl;
             if(prev != nullptr)
             {
-                cout << "555" << endl;
+                // cout << "555" << endl;
                 prev = prev->next;
-                cout << "666" << endl;
+                // cout << "666" << endl;
             }
             // prev = prev->next;
             
-            cout << "444 " << endl;
+            // cout << "444 " << endl;
         }
-        cout << "888"<< endl;
+        // cout << "888"<< endl;
         return nextNode;
     }
 };
 
 int main() 
 {
-    ListNode test1(1);
-    ListNode test2(2, &test1);
+    ListNode test1(5);
+    ListNode test2(4, &test1);
     ListNode test3(3, &test2);
-    ListNode test4(4, &test3);
-    ListNode test5(5, &test4);
+    ListNode test4(2, &test3);
+    ListNode test5(1, &test4);
     ListNode *view = &test5;
 
     Solution sol;
     view = sol.reverseList(view);
-    // while( view != nullptr )
-    // {
-    //     cout << view->val << endl;
-    //     view = view->next;
-    //     cout << "Dammm" << endl;
-    // }
+    while( view != nullptr )
+    {
+        cout << view->val << endl;
+        view = view->next;
+        cout << "Dammm" << endl;
+    }
 
 
 }
