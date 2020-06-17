c356ff8d76ad4f6932920db48fa3143ddc4218a6
ian-SmartAg <isu@smart-ag.com>
2020-06-17 17:46:52 -0500

    Need to test.

--- Leetcode/56_merge_interval.cpp
+++ Leetcode/56_merge_interval.cpp
@@ -2,15 +2,15 @@
 #include <vector>
 using namespace std;
 
-class Solution
-{
-public:
-    vector<vector<int>> merge(vector<vector<int>> &intervals)
-    {
-    }
-};
+// class Solution
+// {
+// public:
+//     vector<vector<int>> merge(vector<vector<int>> &intervals)
+//     {
+//     }
+// };
 
 int main()
 {
-
+    vector< vector<int> > test {{ 1, 3 }, { 2, 6 }, { 8, 10 }, { 15, 18 } };
 }
