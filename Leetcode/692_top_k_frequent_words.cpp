3fdb4470bc36e60b60f79f573a2c2813a7c34e54
ian-SmartAg <isu@smart-ag.com>
2020-06-29 11:59:33 -0500

    692.

--- Leetcode/692_top_k_frequent_words.cpp
+++ Leetcode/692_top_k_frequent_words.cpp
@@ -1,31 +1,50 @@
 #include <iostream>
 #include <vector>
+#include <queue>
 #include <unordered_map>
 
 using namespace std;
+typedef pair<string, int> pi;
 class Solution
 {
-public:
-    vector<string> topKFrequent(vector<string> &words, int k)
-    {
-        unordered_map<string, int> data;
-        for (auto &str : words)
+    public:
+        vector<string> topKFrequent(vector<string> &words, int k)
         {
-            auto it = data.find(str);
-            if(it != data.end())
+            vector<string> ans;
+            unordered_map<string, int> data;
+            for (auto &str : words)
             {
-                data[str] += 1;
+                auto it = data.find(str);
+                if(it != data.end())
+                {
+                    data[str] += 1;
+                }
+                else{
+                    data[str] = 1;
+                }
             }
-            
-        }
-            
+            auto cmp = [](pi A, pi B){
+                if( A.second == B.second) return A.first > B.first;
+
+                return A.second > B.second;
+            };
+            priority_queue< pi, vector<string, pi>, decltype(cmp)> ordered_element(cmp);
+            for(const auto element: data)
+            {
+                ordered_elemnt.push(make_pair(data.first, data.seconf) );
+            }
+
+            return ans;
+                
 
         }
-    }
+
 };
 
 int main()
 {
-    vector<string> 
+    vector<string> test{"i", "love", "leetcode", "i", "love", "coding"};
+    Solution sol;
 
+    
 }
