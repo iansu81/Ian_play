1a5cc447b81abc8996a75301cf320b0c12479d83
ian-SmartAg <isu@smart-ag.com>
2020-07-10 23:03:17 -0500

    994 half way.

---
+++ Leetcode/238_product_of_arry_except_itself.cpp
@@ -0,0 +1,20 @@
+#include <vector>
+#include <iostream>
+
+using namespace std;
+
+
+
+class Solution
+{
+public:
+    vector<int> productExceptSelf(vector<int> &nums)
+    {
+    }
+};
+
+
+int main(){
+
+    
+}
--- Leetcode/994_rotting_orange.cpp
+++ Leetcode/994_rotting_orange.cpp
@@ -20,11 +20,30 @@ public:
                 if(grid[i][j] == 2) rotten_list.push(make_pair(i,j));
             }
         }
-        while(rotten_list.empty())
+        while( !rotten_list.empty() && fresh ) 
+        { 
+            int size = rotten_list.size();
+            while(size--)
+            {
+                pair<int, int> center = rotten_list.front();
+                rotten_list.pop();
+                pair<int, int> right_point (center.first, center.second+1);
+                pair<int, int> up_point (center.first-1, center.second);
+                pair<int, int> left_point (center.first, center)
+                pair<int, int> _point =
+                pair<int, int> up_point =
+            }
+
+
+
+        }
+        return fresh 
     }
 };
 
 int main(){
+
+    vector test1
 
 
 }
