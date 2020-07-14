3840dc23a8d13f4d365162651ec67b8276da98ec
Ian Su <iansu81@gmail.com>
2020-07-13 23:34:18 -0500

    test.

--- Leetcode/994_rotting_orange.cpp
+++ Leetcode/994_rotting_orange.cpp
@@ -17,7 +17,7 @@ public:
             for(int j = 0; j < grid[i].size(); j++)
             {
                 if(grid[i][j] == 1) fresh++;
-                if(grid[i][j] == 2) rotten_list.push(make_pair(i,j));
+                if(grid[i][j] == 2) rotten_list.push_back(make_pair(i,j));
             }
         }
         while( !rotten_list.empty() && fresh ) 
@@ -28,42 +28,51 @@ public:
                 pair<int, int> center = rotten_list.front();
                 rotten_list.pop();
                 // Check up
-                if ((center.first - 1) >= 0 && grid[center.first - 1][center.second] == 1)
+                if ((center.first - 1) >= 0 && grid[center.first-1][center.second] == 1)
                 {
-                    
+                    fresh--;
+                    grid[center.first - 1][center.second] = 2; 
+                    rotten_list.push_back(center.first - 1, center.second);
                 }
                 // Check right
-                if ((center.second + 1) <= grid[0].size() && grid[center.first][center.second+1] == 1)
+                if ((center.second + 1) < grid[0].size() && grid[center.first][center.second+1] == 1)
                 {
+                    resh--;
+                    grid[center.first][center.second+1] = 2; 
+                    rotten_list.push_back(center.first, center.second+1);
                 }
 
                 // Check left
-                if ((center.second - 1) <= 0 && grid[center.first][center.second - 1] == 1)
+                if ((center.second - 1) >= 0 && grid[center.first][center.second - 1] == 1)
                 {
+                    resh--;
+                    grid[center.first][center.second-1] = 2; 
+                    rotten_list.push_back(center.first, center.second-1);
                 }
 
                 //Check down
-                if ((center.first + 1) <= grid.size() && grid[center.first + 1][center.second] == 1)
+                if ((center.first + 1) < grid.size() && grid[center.first+1][center.second] == 1)
                 {
+                    resh--;
+                    grid[center.first+1][center.second] = 2; 
+                    rotten_list.push_back(center.first+1,center.second);
                 }
-
-                    pair<int, int> right_point(center.first, center.second + 1);
-                pair<int, int> up_point (center.first-1, center.second);
-                pair<int, int> left_point (center.first, center)
-                pair<int, int> _point =
-                pair<int, int> up_point =
             }
-
+            days++;
 
 
         }
-        return fresh 
+        return fresh ? -1: days;
     }
 };
 
 int main(){
 
-    vector test1
+    vector<int> test1{2,1,1};
+    vector<int> test2{1,1,0};
+    vector<int> test3{0,1,1};
+    vector<vector<int>> test = {};
+    
 
 
 }
