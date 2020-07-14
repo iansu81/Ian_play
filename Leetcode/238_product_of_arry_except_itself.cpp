2ada2b4db6cb68c2c170abe162214fbe70276c90
ian-SmartAg <isu@smart-ag.com>
2020-07-14 16:42:58 -0500

    238 done.

--- Leetcode/238_product_of_arry_except_itself.cpp
+++ Leetcode/238_product_of_arry_except_itself.cpp
@@ -10,11 +10,36 @@ class Solution
 public:
     vector<int> productExceptSelf(vector<int> &nums)
     {
+        vector<int> left(nums.size(), 1);
+        vector<int> right(nums.size(), 1);
+        vector<int> result;
+
+        //紀錄在所有在index i左邊元素的乘積
+        for(int i = 1; i < nums.size(); ++i)
+        {
+            left[i] = left[i-1] * nums[i-1];
+        }
+        //紀錄在所有在index i右邊元素的乘積
+        for(int i = nums.size() - 2; i >= 0; --i )
+        {
+            right[i] = right[i+1] * nums[i+1] ;
+        }
+        //所有左邊的乘積＊所有右邊的乘積即為答案
+        for(int i = 0; i < nums.size(); ++i)
+        {
+            result.push_back(left[i]*right[i]);
+        }
+        return result;
+
     }
 };
 
 
 int main(){
-
-    
+    vector<int> test { 1, 2, 3, 4 };
+    Solution sol;
+    vector<int> result = sol.productExceptSelf(test);
+    for(int i =0 ; i < result.size(); i++){
+        cout << result[i] << endl;
+    }
 }
