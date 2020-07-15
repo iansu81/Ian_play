1192be99a3199131e7d0007eab0debb01b4abad7
ian-SmartAg <isu@smart-ag.com>
2020-07-15 17:24:45 -0500

    starting p2.py.

---
+++ int/p2.py
@@ -0,0 +1,37 @@
+from Queue import PriorityQueue
+
+
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+
+@dataclass
+class TimeData:
+    time: float  # in seconds
+    value: float
+
+class Solution(object):
+    def mergeKLists(self, lists):
+        """
+        :type lists: List[ListNode]
+        :rtype: ListNode
+        """
+        head = point = ListNode(0)
+        q = PriorityQueue()
+        for l in lists:
+            if l:
+                q.put((l.val, l))
+        while not q.empty():
+            val, node = q.get()
+            point.next = ListNode(val)
+            point = point.next
+            node = node.next
+            if node:
+                q.put((node.val, node))
+        return head.next
+
+
+if __name__ == '__main__':
+    data1 = TimeData()
