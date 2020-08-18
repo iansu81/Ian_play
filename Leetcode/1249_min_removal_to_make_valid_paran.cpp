e8bc24e442afec74ed7ad40632064a27b0e7f0cd
ian-SmartAg <isu@smart-ag.com>
2020-08-18 10:19:11 -0500

    1249 Done.

--- Leetcode/1249_min_removal_to_make_valid_paran.cpp
+++ Leetcode/1249_min_removal_to_make_valid_paran.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <string>
 #include <algorithm>
+#include <stack>
 
 using namespace std;
 
@@ -9,21 +10,34 @@ class Solution
 public:
     string minRemoveToMakeValid(string s)
     {
-        int close = count(s.begin(), s.end(), ')');
-        int open = 0;
-        string ans; 
-        for(const auto& c: s)
+        //利用一個stack來記錄所有’（‘位置
+        stack<int> record;
+        for(int i = 0; i < s.size(); i++)
         {
-            if(c == '('){
-                if(open == close) continue;
-                open++
-            }
-            else if(c == ')')
-            {
-
+            if(s[i] == '(') record.push(i);
+            if(s[i] == ')'){
+                //當stack不為空,遇到‘)’的時候代表是一個合法的')'
+                //不然就是一個不合法的')'.在這邊把')'標記為‘＊’
+                if(!record.empty())
+                {
+                    record.pop();
+                }
+                else{
+                    s[i] = '*';
+                }
             }
         }
-
+        // 處理剩下不合法的'(',把他們全部標記為'*'
+        while(!record.empty())
+        {
+            int index = record.top();
+            record.pop();
+            s[index] = '*'; 
+            
+        }
+        // 把所有不合法的‘＊’都刪除掉
+        s.erase(remove(s.begin(), s.end(), '*'), s.end());
+        return s;
     }
 };
 
