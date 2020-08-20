94784306f97afffcd649f692443525f25a364834
ian-SmartAg <isu@smart-ag.com>
2020-08-20 11:38:45 -0500

    22 done.

--- Leetcode/22_generate_paren.cpp
+++ Leetcode/22_generate_paren.cpp
@@ -7,15 +7,16 @@ using namespace std;
 class Solution
 {
 public:
-    void dfs(int l, int r, string s, vector<string> result)
+    void dfs(int l, int r, string s, vector<string>& result)
     {   
+        //遍歷完所有的parenthesis就儲存起來
         if(l == 0 && r==0)
         {
             result.push_back(s);
             return;
         }
-        if( r > 0) dfs(l, r-1, s)
-        if( l > 0)
+        if( r > 0) dfs(l, r-1, s+')', result);
+        if( l > 0) dfs(l-1, r+1, s+'(', result);
 
     }
     vector<string> generateParenthesis(int n)
@@ -32,6 +33,7 @@ int main()
 
     Solution sol;
     vector<string> result = sol.generateParenthesis(3);
+    cout << "Size: " << result.size() << endl;
     for(int i = 0 ; i < result.size(); i++)
     {
         cout << result[i] << endl;
