5748c09dc3b8d0122398b5bba6733f97e03fe730
Ian Su <iansu81@gmail.com>
2020-09-03 22:18:51 -0500

    394 start.

---
+++ Leetcode/394_decoded_string.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+using namespace std;
+
+
+
+class Solution {
+public:
+    string decodeString(string s) {
+        
+    }
+};
+
+
+int main()
+{
+    string test = "3[a]2[bc]";
+    Solution sol;
+    string ans = sol.decodeString(test);
+    cout << "Ans: " << ans << endl;
+
+}
