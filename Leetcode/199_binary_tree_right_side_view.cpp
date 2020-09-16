7e219f2faf157f294f44a17fb74584c43c1be550
ian-SmartAg <isu@smart-ag.com>
2020-09-16 17:00:26 -0500

    199 done.

--- Leetcode/199_binary_tree_right_side_view.cpp
+++ Leetcode/199_binary_tree_right_side_view.cpp
@@ -14,6 +14,10 @@ using namespace std;
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
+// 從Tree 的最上面開始找做dfs, 然後每次先找右邊的node, 因為是從右邊看, 如果有右邊的node就一定是我們要找的數, 所以每次dfs下一層 當level = result.size()的時候
+// ex: level = 0 , result = 0, 這時候目前尋訪的這元素就是這一層最右邊的元素也是我們想要的元素. 右邊的尋訪完了再尋訪左邊
+
+
 class Solution
 {
 public:
@@ -25,13 +29,15 @@ public:
     }
     void dfs(TreeNode *root, int level, vector<int> &result)
     {
-        if(level == result.size() && root != nullptr)
+        if( root == nullptr)
+        {
+            return;
+        }
+        if(level == result.size())
         {
             result.push_back(root->val);
-            dfs(root->right, level + 1, result);
-            dfs(root->left, level + 1, result);
         }
-
-
+        dfs(root->right, level + 1, result);
+        dfs(root->left, level + 1, result);
     }
 };
