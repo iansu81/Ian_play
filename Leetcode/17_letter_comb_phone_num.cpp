b26d0f7719676f32335979ebab80c46e448de62d
ian-SmartAg <isu@smart-ag.com>
2020-09-17 18:10:12 -0500

    17,49 done.

--- Leetcode/17_letter_comb_phone_num.cpp
+++ Leetcode/17_letter_comb_phone_num.cpp
@@ -9,6 +9,11 @@ public:
     vector<string> letterCombinations(string digits)
     {
         vector<string> result;
+        if(digits.size() == 0)
+        {
+            return result;
+        }
+        // 建立一個table去求所有按鍵的字母
         unordered_map<char, vector<char>> table(
             {
                 {'2', {'a', 'b', 'c'}},
@@ -21,23 +26,23 @@ public:
                 {'9', {'w', 'x', 'y','z'}}
             }
         );
-
+        string tmp_record;
+        helper(table, digits, 0, result, tmp_record);
         return result;
     }
-    void helper(unordered_map<char, vector<char>> &table, string digit, vector<string> &result, string &record){
-        if(record.size() == digit.size())
+    // 利用int l去遍歷所有digit,每次push進去之後記得再pop讓record recover
+    void helper(unordered_map<char, vector<char>> &table, string digits, int l, vector<string> &result, string &record){
+        if(l >= digits.size())
         {
             result.push_back(record);
             return;
         }
-        for(const auto& d: digit)
+        // 這邊記得不要在一個for loop去遍歷digit, 只要考慮index l,不然會出現"dd, de,df, ee,ff" 這種重複
+        for(const auto& c: table[digits[l]])
         {
-            for(const auto& c: table[d])
-            {
-                
-                record.push_back(c);
-                helper(table)
-            }
+            record.push_back(c);
+            helper(table, digits, l+1, result, record);
+            record.pop_back();
         }
     }
 
--- Leetcode/49_group_anagrams.cpp
+++ Leetcode/49_group_anagrams.cpp
@@ -3,21 +3,52 @@
 #include <iostream>
 #include <vector>
 #include <unordered_map>
+#include <algorithm>
 using namespace std;
 
+
+// 1. 建立一個hash map, key是sort之後的string, value是在所有在strings裡sort之後是相同string的index
+// ex: [aet, ate] => sort之後都是aet => hash[aet] = [0,1]
+// 2. 之後遍歷所有在hash map, 把sort之後相同的string 都push到vector裡再push 到ans的vector裡面
 
 class Solution
 {
 public:
     vector<vector<string>> groupAnagrams(vector<string> &strs)
     {
+        vector<vector<string>> ans;
+        unordered_map<string, vector<int>> str_record;
+        for(int i = 0 ; i < strs.size() ; i++)
+        {
+            string sorted_str = strs[i];
+            sort(sorted_str.begin(), sorted_str.end());
+            str_record[sorted_str].push_back(i);
+        }
+        for(auto kv: str_record)
+        {
+            vector<string> sub_ans;
+            for(int i = 0; i < kv.second.size(); i ++)
+            {
+                sub_ans.push_back(strs[kv.second[i]]);
+            }
+            ans.push_back(sub_ans);
+        }
+        return ans;
     }
 };
 
 
 int main()
 {
-
-
-
+    vector<string> test_strs {  "eat", "tea", "tan", "ate", "nat", "bat" };
+    Solution sol;
+    vector<vector<string>> result = sol.groupAnagrams(test_strs);
+    for (int i = 0; i < result.size(); i++)
+    {
+        for (int j = 0; j < result[i].size(); j++)
+        {
+            cout << result[i][j] << ',';
+        }
+        cout << endl;
+    }
 }
