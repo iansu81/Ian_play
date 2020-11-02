526955ae2bf6f0fc9f04c2730fdb6e685daee0f0
ian-SmartAg <isu@smart-ag.com>
2020-11-02 16:52:07 -0600

    1041 done.

--- Leetcode/1041_robot_bounded_in_a_circle.cpp
+++ Leetcode/1041_robot_bounded_in_a_circle.cpp
@@ -12,16 +12,27 @@ public:
         int x = 0;
         int y = 0;
         int direction = 0;
+        // 四種移動, 設定index = 0 的時候是向上的, index = 1 向右, index = 2 向下, index = 3 向左
         vector< vector<int> > move{ {0,1}, {1,0}, {0,-1}, {-1,0}};
-        for(const auto＆ e: instructions)
+        for(const auto &e: instructions)
         {
-            if( e == 'R') direction = (direction + 1) % 4;
-            else if ( e == 'L' ) direction = (direction + 3) % 4;
+            // 往右轉90度 direction +1
+            if( e == 'R') 
+            {
+                direction = (direction + 1) % 4;
+            }
+            //  往左轉90度 因為不想要有負的, 所以＋3 再 mod
+            else if ( e == 'L' ) 
+            {
+                direction = (direction + 3) % 4;
+            }
+            
             else{
-                x = move[direction][0];
-                y = move[direction][1];
+                x = x + move[direction][0];
+                y = y + move[direction][1];
             }
         }
+        // 確認最後有沒有回到原點, 或者方向不是向上, 不是向上的話在2個iteration或4個iteration之後就會繞回原本的circle,所以也符合規定
         return ((x == 0 && y == 0) || direction != 0);
         
     }
@@ -29,7 +40,7 @@ public:
 
 int main()
 {
-    string test = "GGLLGG";
+    string test = "GLRLLGLL";
     Solution sol;
     cout << sol.isRobotBounded(test) << endl;
 
