401e5ae0814333ae64a7720a0740dd83b49a8f7b
ian-SmartAg <isu@smart-ag.com>
2021-01-19 15:14:48 -0600

    207 needs commit.

--- Leetcode/207_course_schedule.cpp
+++ Leetcode/207_course_schedule.cpp
@@ -9,12 +9,14 @@ public:
         vector<vector<int>> req_graph(numCourses, vector<int>() );
         vector<int> course_state(numCourses, 0); // 0: has not been visited, 1: visting, 2: visited
         make_graph(req_graph, prerequisites);
-        
-        // return toposort(0, req_graph, course_state );
+        for(int i = 0 ; i < numCourses; i++ )
+        {
+            if (!toposort(i, req_graph, course_state)) return false;
+        }
         return true;
     }
 
-
+    // 建立一個graph, key: 課程, value: 先修課
     void make_graph(vector<vector<int>>& graph, vector<vector<int>>& prerequisites)
     {
         for(const auto& course: prerequisites)
@@ -23,29 +25,30 @@ public:
         }
     }
 
-    // bool toposort(int visiting_course, vector<vector<int>>& graph, vector<int>＆ course_state)
-    // {
-    //     if(course_state[visiting_course] == 1) return false;
-    //     course_state[visiting_course] = 1;
-    //     bool ans = true;
-    //     for(const auto& c: graph[visiting_course])
-    //     {
-    //         ans = toposort(c, graph, course_state);
-    //         if(!ans) return false;
-    //     }
+    bool toposort(int visiting_course, vector<vector<int>> &graph, vector<int> &course_state)
+    {
+        if(course_state[visiting_course] == 2) return true;
+        if(course_state[visiting_course] == 1) return false;
+        course_state[visiting_course] = 1;
+        bool ans = true;
+        for(const auto& c: graph[visiting_course])
+        {
+            ans = toposort(c, graph, course_state);
+            if(!ans) cout << "AAAAAAA" << endl;
+            if(!ans) return false;
+        }
 
-    //     course_state[visiting_course] = 2;
-    //     return ans;
-    // }
+        course_state[visiting_course] = 2;
+        return ans;
+    }
 
 };
-
 
 int main()
 {
     Solution sol;
-    vector<vector<int>> test{{1,0}};
+    vector<vector<int>> test{{2, 0}, {1, 0}, {3, 1}, {3, 2}, {1, 3}};
 
-    cout << "Ans: " << sol.canFinish(2, test) << endl;
+    cout << "Ans: " << sol.canFinish(4, test) << endl;
 
 }
