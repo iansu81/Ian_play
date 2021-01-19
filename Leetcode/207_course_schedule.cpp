237a25a933c069987a7a6834879a17858ae24177
ian-SmartAg <isu@smart-ag.com>
2021-01-19 17:19:33 -0600

    207 done.

--- Leetcode/207_course_schedule.cpp
+++ Leetcode/207_course_schedule.cpp
@@ -9,6 +9,8 @@ public:
         vector<vector<int>> req_graph(numCourses, vector<int>() );
         vector<int> course_state(numCourses, 0); // 0: has not been visited, 1: visting, 2: visited
         make_graph(req_graph, prerequisites);
+
+        // 記得要遍歷所有的course
         for(int i = 0 ; i < numCourses; i++ )
         {
             if (!toposort(i, req_graph, course_state)) return false;
@@ -25,8 +27,13 @@ public:
         }
     }
 
+    // topological sort 每次得到的解不一定一樣, 但是如果有環, 就不能做topological sort,
+    // 所以第一次visit 一個node的時候, 把這個node 的state mark成1, 然後往他的先修課下去找, 如果找先修課又找回自己, 就代表有環
+
     bool toposort(int visiting_course, vector<vector<int>> &graph, vector<int> &course_state)
     {
+        // 當一個course被mark成state 2 代表他跟他的先修課, 都是沒有問題的, 就不用再繼續找下去, 直接return true
+        // 繼續找下去也是浪費時間
         if(course_state[visiting_course] == 2) return true;
         if(course_state[visiting_course] == 1) return false;
         course_state[visiting_course] = 1;
@@ -34,7 +41,6 @@ public:
         for(const auto& c: graph[visiting_course])
         {
             ans = toposort(c, graph, course_state);
-            if(!ans) cout << "AAAAAAA" << endl;
             if(!ans) return false;
         }
 
