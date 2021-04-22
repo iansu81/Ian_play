bdf32d8d358a496b397024ca446d4a5fa81344bc
Ian Su <iansu81@gmail.com>
2021-04-21 20:52:25 -0700

    739 done.

--- Leetcode/739_daily_temperature.cpp
+++ Leetcode/739_daily_temperature.cpp
@@ -5,44 +5,34 @@
 #include <algorithm>
 #include <stack>
 using namespace std;
+
+
+// 創建一個嚴格遞減的stack來記錄從最大到最小
+// 當遍歷的元素比stack中的元素還要大的時候, 就要把元素pop出來直到比遍歷的元素小或空
+// 如果pop完發現是空的, 代表後面沒有元素比正在遍歷的元素還要大, 所以這個元素就應該是0
+// 否則就是stack中離這個元素最近的比較大的元素的index-i 
 class Solution
 {
     public:
         vector<int> dailyTemperature(vector<int>& T)
         {
             stack<pair<int,int>> monodecreasing;
-            vector<int> ans;
-            ans.push_back(0);
+            vector<int> ans(T.size(), -1);
+            ans[T.size()-1] = 0;
             monodecreasing.push(make_pair(T.back(), T.size()-1));
             for(int i = T.size()-2; i >= 0; i--)
             {
-                if(T[i] < monodecreasing.top().first)
-                {
-                    ans.push_back(monodecreasing.top().second - i);
-                    monodecreasing.push(make_pair(T[i], i));
-                }
-                else{
-                    while(T[i] >= monodecreasing.top().first)
-                    {
-                        monodecreasing.pop();
-                    }
-                    if(!monodecreasing.empty())
-                    {
-                        ans.push_back(monodecreasing.top().second - i);
-                    }
-                    else
-                    {
-                        ans.push_back(0);
-                    }
-                    monodecreasing.push(make_pair(T[i], i));
-                }
+                while(!monodecreasing.empty() && T[i] >= monodecreasing.top().first) monodecreasing.pop();
+                if(!monodecreasing.empty())  ans[i] = monodecreasing.top().second - i;
+                else ans[i] = 0;
+                monodecreasing.push(make_pair(T[i], i));
+    
             }
-            reverse(ans.begin(), ans.end());
             return ans;
 
         }
 
-}
+};
 
 
 int main()
@@ -50,8 +40,11 @@ int main()
     vector<int> test{73,74,75, 71, 69,72,76,73};
     // [1,1,4,2,1,1,0,0]
     Solution sol;
-
-    cout << endl;
+    vector<int> ans = sol.dailyTemperature(test);
+    for(auto& e: ans)
+    {
+        cout << e << ',';
+    }
 
 
 }
