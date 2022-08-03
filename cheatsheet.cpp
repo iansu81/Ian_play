ef49f728333e7eb2e4ff4a908f8f666fbe836366
Ian Su <iansu81@gmail.com>
2022-08-03 11:20:55 -0700

    Note

--- cheatsheet.cpp
+++ cheatsheet.cpp
@@ -17,3 +17,15 @@ sort(intervals.begin(), intervals.end(), [](vector<int> a, vector<int> b)
 {
     return a[0] < b[0];
 });
+
+
+
+如果是在嚴格遞增陣列中:
+lower_bound( begin,end,num)：從陣列的begin位置到end-1位置二分查詢第一個大於或等於num的數字，找到返回該數字的地址，不存在則返回end。通過返回的地址減去起始地址begin,得到找到數字在陣列中的下標。
+
+upper_bound( begin,end,num)：從陣列的begin位置到end-1位置二分查詢第一個大於num的數字，找到返回該數字的地址，不存在則返回end。通過返回的地址減去起始地址begin,得到找到數字在陣列中的下標。
+
+如果是在從大到小的排序陣列中：
+lower_bound( begin,end,num,greater() ):從陣列的begin位置到end-1位置二分查詢第一個小於或等於num的數字，找到返回該數字的地址，不存在則返回end。通過返回的地址減去起始地址begin,得到找到數字在陣列中的下標。
+
+upper_bound( begin,end,num,greater() ):從陣列的begin位置到end-1位置二分查詢第一個小於num的數字，找到返回該數字的地址，不存在則返回end。通過返回的地址減去起始地址begin,得到找到數字在陣列中的下標。
